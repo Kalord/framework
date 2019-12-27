86cf69d9dbeb16ba09080be6e81242ec0bbd1f1a
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2019-12-28 00:19:27 +0800

    Added test system

---
+++ framework/tests/TestForTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class TestForTest extends TestCase
+{
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     * @param array $stack
+     * @return array
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     * @param array $stack
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+}
