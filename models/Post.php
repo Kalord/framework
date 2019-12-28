feb3ac093443eb5087f0f4a18524668de99d69d3
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2019-12-28 19:49:18 +0800

    Update test model

--- models/Post.php
+++ models/Post.php
@@ -4,8 +4,19 @@ namespace app\models;
 
 use app\framework\core\ActiveRecord;
 
+/**
+ * @property int $id;
+ * @property string $title
+ * @property string $content
+ * @package app\models
+ */
 class Post extends ActiveRecord
 {
+    public static function pathToConfig()
+    {
+        return '../configs/db-test.php';
+    }
+
     public function getId()
     {
         return $this->id;
