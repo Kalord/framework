e6c6c768fb0497d506baba52211e7c8e0980a8d2
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-02 02:52:34 +0800

    Added logic for work with assets

--- views/layout/main.php
+++ views/layout/main.php
@@ -1,14 +1,20 @@
 <?php
 /* @var string $content */
+
+use app\assets\AssetBase;
+
+$asset = new AssetBase();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Framework</title>
+    <?= $asset->getCss() ?>
 </head>
 <body>
     <header><h1>Header</h1></header>
     <?= $content ?>
     <footer><h1>Footer</h1></footer>
+    <?= $asset->getJs() ?>
 </body>
 </html>
