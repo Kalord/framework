fd0b75a7854c9d0e04f5aac4a7ffdf82810e8ccd
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-17 23:37:15 +0800

    Added method updateLastColumn

--- framework/components/db/ddl/ColumnBuilder.php
+++ framework/components/db/ddl/ColumnBuilder.php
@@ -31,6 +31,11 @@ class ColumnBuilder
         $this->columns[] = $column;
     }
 
+    private function updateLastColumn($sql)
+    {
+        $this->columns[count($this->columns) - 1] .= $sql;
+    }
+
     public function getColumns()
     {
         return $this->columns;
@@ -44,6 +49,45 @@ class ColumnBuilder
      */
     public function createPrimaryKey($type = self::TYPE_INT)
     {
-        $this->addColumn("$type NOT NULL PRIMARY KEY AUTO_INCREMENT");
+        $this->addColumn("$type NOT NULL PRIMARY KEY AUTO_INCREMENT ");
+    }
+
+    public function createInteger($length)
+    {
+        $this->addColumn("INT($length) ");
+    }
+
+    /**
+     * Создание колонки типа varchar
+     * 
+     * @param int $length
+     * @return void
+     */
+    public function createVarchar($length)
+    {
+        $this->addColumn("VARCHAR($length) ");
+    }
+
+    public function createText($length)
+    {
+        $this->addColumn("TEXT($length) ");
+    }
+
+    public function createCurrentTimestamp()
+    {
+        $this->addColumn("DATETIME DEFAULT CURRENT_TIMESTAMP ");
+    }
+
+    /**
+     * Добавление к последней колонки опции NOT NULL
+     */
+    public function notNull()
+    {
+        $this->updateLastColumn('NOT NULL ');
+    }
+
+    public function defaultValue($value)
+    {
+        $this->updateLastColumn("DEFAULT $value ");
     }
 }
